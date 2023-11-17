cb7e74d4da00c1c11964bdce45304fe03cb2db0f
lagardeheidi <lagarde.heidi34@gmail.com>
2023-11-17 18:35:19 +0100

    rajout argument fonction magic system

--- src/functions.hpp
+++ src/functions.hpp
@@ -5,7 +5,7 @@ Eigen::VectorXd random_point();
 Eigen::VectorXd random_infinite_point();
 Eigen::MatrixXd random_points_matrix(const size_t &n);
 Eigen::VectorXd pt_controle(const Eigen::VectorXd &v);
-Eigen::MatrixXd matrice_systeme(const Eigen::MatrixXd &m);
+Eigen::MatrixXd matrice_systeme(const Eigen::MatrixXd &m, const size_t &n);
 Eigen::VectorXd resolve_conic(const Eigen::MatrixXd &m);
 
 #endif
